ea1baf97c4e37896fc0171ce6b6ff10d1a355610
Arvand Molla <mr.arvand@gmail.com>
2021-12-08 16:33:28 +0100

    apis

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,26 @@
 import express, { Application } from "express";
 import cors from "cors";
-import mongoose, { ConnectOptions } from "mongoose";
-import { ConnectionOptions } from "tls";
-import eventRouter from './api/eventsAPI'
+import mongoose from "mongoose";
+import eventRouter from "./api/eventsAPI";
 
-const app: Application = express()
+const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(eventRouter)
+app.use(eventRouter);
 
-const port: number = 5000; 
-// mongoose
-//   .connect("mongodb+srv://ArvandAdmin:Shahab44@cluster0.c8kh0.mongodb.net/exopulse?authSource=admin&replicaSet=atlas-h3p6db-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//   } as ConnectOptions, ()=> {
-//     app.listen(port, () => {
-//     console.log("✅✅✅ Running on port", port);
-//   }
-//   )})
+const port: number = 5000;
 
 if (!process.env.MONGO_CONNECTION) {
-  throw new Error("No Mongo Url defined!")
+  throw new Error("No Mongo Url defined!");
 }
-  
-mongoose.connect(process.env.MONGO_CONNECTION).then( () => {
-  app.listen(port, () => {
-    console.log("✅✅✅ Running on port", port);
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION)
+  .then(() => {
+    app.listen(port, () => {
+      console.log("✅✅✅ Running on port", port);
+    });
   })
-
-})
-
-  
-
- 
+  .catch((err) => console.log(err));
