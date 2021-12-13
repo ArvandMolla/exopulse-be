d515a50ffcf55d788308d46504667e42c6f4ec8c
Arvand Molla <mr.arvand@gmail.com>
2021-12-13 16:04:18 +0100

    unit testing for apis

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -9,29 +9,37 @@ const request = supertest(server);
 
 describe("Testing suit #1", () => {
   beforeAll((done) => {
-    mongoose.connect(process.env.MONGO_CONNECTION!).then(() => {
+    mongoose.connect(process.env.MONGO_TEST_CONNECTION!).then(() => {
       console.log("Connected to Atlas");
       done();
     });
   });
-  it("should test the /test api works correctly", async () => {
-    const response = await request.get("/api/test");
-    expect(response.status).toBe(200);
-  });
-  it("should test that getting all events from api/event returns 200", async () => {
+
+  it("should test that getting all events from api/event is working correctly", async () => {
     const response = await request.get("/api/event");
 
     expect(response.status).toBe(200);
+    expect(response.body).toBeDefined();
   });
 
-  //   afterAll((done) => {
-  //     mongoose.connection.dropDatabase().then(() => {
-  //       mongoose.connection.close().then(done);
-  //     });
-  //   });
+  it("should test that when we add a new event, we can retrieve it right away", async () => {
+    const validEvent = {
+      sam: 888,
+      payload: "test event 888",
+    };
+
+    const response = await request.post("/api/event").send(validEvent);
+    expect(response.status).toBe(201);
+    expect(response.body._id).toBeDefined();
+
+    const response2 = await request.get("/api/event/" + response.body._id);
+    expect(response2.status).toBe(200);
+    expect(response2.body.sam).toEqual(validEvent.sam);
+  });
 
   afterAll((done) => {
-    mongoose.connection.close();
-    done();
+    mongoose.connection.dropDatabase().then(() => {
+      mongoose.connection.close().then(done);
+    });
   });
 });
